11863203aae7326ae2ea41e6cd35ddfbf7023d19
dried021 <dried021@gmail.com>
2025-04-03 19:42:41 +0900

    [2025.04.03] board 수정

--- src/main/java/com/project/repository/BoardMapper.java
+++ src/main/java/com/project/repository/BoardMapper.java
@@ -1,8 +1,13 @@
 package com.project.repository;
 
+import java.util.List;
+import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
+
+import com.project.model.BoardDto;
 
 @Mapper
 public interface BoardMapper {
     public int getCount();
+    public List<BoardDto> getArticles(Map<String, Integer> map);
 }
