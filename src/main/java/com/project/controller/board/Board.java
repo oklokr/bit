7b083d7bcd4b1f1c010b5dac1bddea9d1de77ccc
dried021 <dried021@gmail.com>
2025-04-04 09:20:54 +0900

    [2025.04.04] board 복구

--- src/main/java/com/project/controller/board/Board.java
+++ src/main/java/com/project/controller/board/Board.java
@@ -1,5 +1,9 @@
 package com.project.controller.board;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,11 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.model.BoardDto;
+import com.project.service.BoardService;
 
 @Controller
 public class Board {
     @Autowired
-    private BoardDto boardDao;
+    private BoardService boardDao;
 
     @GetMapping("/board")
     public String board(@RequestParam(required=false) String pageNum, Model model) {
@@ -24,8 +29,54 @@ public class Board {
         int end=0;
         int number=0;   //출력되는 글번호
         int pageCount=0; //페이지 개수
+        int startPage = 0; //출력 페이지 시작 
+        int endPage = 0; //출력 페이지 끝
 
-        
+        count = boardDao.getCount();
+
+        if (pageNum == null || pageNum.equals("")){
+            pageNum="1";
+        }
+        currentPage = Integer.parseInt(pageNum);
+        start=(currentPage-1)*pageSize+1;
+        end = start+pageSize -1; 
+        if(end>count){
+            end=count;
+        }
+
+        number=count-(currentPage-1) * pageSize;
+
+        pageCount = (count / pageSize) + (count % pageSize > 0 ? 1 : 0);
+        startPage=(currentPage/pageBlock)*pageBlock+1;
+        if(currentPage%pageBlock==0)
+            startPage-=pageBlock;
+        endPage=startPage+pageBlock-1;
+        if(endPage>pageCount)
+            endPage = pageCount;
+        model.addAttribute("pageBlock", pageBlock);
+        model.addAttribute("count", count);
+        model.addAttribute("number", number);
+        model.addAttribute("pageNum", pageNum);
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("pageCount", pageCount);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+
+        if (count>0){
+            Map<String,Integer> map = new HashMap<String,Integer>();
+            start -= 1;
+            map.put("start", start);
+            //map.put("end", end);
+            map.put("pageSize", pageSize);
+
+            List<BoardDto> dtos = boardDao.getArticles(map);
+            model.addAttribute("dtos", dtos);
+            for (BoardDto dto:dtos){
+                System.out.println(dto.getTitle());
+
+            }
+        }
+        System.out.println(start+end);
         model.addAttribute("title", "main page");
         model.addAttribute("contentPage", "/WEB-INF/page/board/board.jsp");
         return "layout/app";
