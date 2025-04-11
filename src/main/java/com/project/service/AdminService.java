a3af9be04095c0ca5742bdcd8f9136e7c581bdd7
dried021 <dried021@gmail.com>
2025-04-11 12:00:32 +0900

    [2025.04.11] detail.jsp 수정

--- src/main/java/com/project/service/AdminService.java
+++ src/main/java/com/project/service/AdminService.java
@@ -1,11 +1,8 @@
 package com.project.service;
-
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.project.model.UserDto;
 import com.project.repository.AdminMapper;
 
