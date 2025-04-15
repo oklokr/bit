fd2bdfad187109f3282658774d053feba80d1cfd
dried021 <dried021@gmail.com>
2025-04-15 10:27:40 +0900

    [2025.04.15] 불필요 import 제거거

--- src/main/java/com/project/controller/admin/AdminDetail.java
+++ src/main/java/com/project/controller/admin/AdminDetail.java
@@ -1,8 +1,5 @@
 package com.project.controller.admin;
-
-import java.util.HashMap;
 import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
